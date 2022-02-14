6879503e71c0f6a92cf490951af0ea81eab03fc7
MAPHAE <82727702+MAPHAE@users.noreply.github.com>
2022-02-14 02:47:48 -0800

    Create setup.sh
    creating a user with super user privileges.

--- setup.sh
+++ setup.sh
@@ -25,4 +25,21 @@ sudo su postgres
 
 sudo -i -u postgres psql -c "CREATE USER mob_db_user WITH PASSWORD ' mob_db_pass';"
 
+sudo -u postgres bash -c "psql -c \" CREATE USER mob_db_user WITH PASSWORD '123';\""
+sudo -u postgres bash -c "psql -c \"ALTER USER mob_app_user WITH SUPERUSER ;\""
+sudo -u postgres bash -c "psql -c \"mkdir project ;\""
+
+export PGPASSWORD='FILL ME'
+
+echo "Dropping $1"
+/usr/local/pgsql/bin/dropdb $1
+
+echo "Creating $1"
+/usr/local/pgsql/bin/createdb -U postgres $1 -E UTF8
+
+echo "Importing $2 into $1"
+/usr/local/pgsql/bin/psql -U postgres $1 < $2
+
+echo "All done!!"
+
 
